4dd062dd829140b55b7fda336af82833366aaa82
Kevin Deisz <kevin.deisz@gmail.com>
2017-04-27 18:57:07 -0400

    Even faster

--- lib/iseq_rails_tools.rb
+++ lib/iseq_rails_tools.rb
@@ -14,24 +14,18 @@ if !Rails.env.test? || IseqRailsTools.respond_to?(:internal?)
       def clear
         Dir.glob(File.join(iseq_dir, '**/*.yarb')) { |path| File.delete(path) }
       end
-
-      private
-
-      def find_root
-        root =
-          caller_locations.detect do |location|
-            path = location.absolute_path || location.path
-            if path != __FILE__ && path !~ %r{bundler[\w.-]*/lib/bundler}
-              break File.dirname(path)
-            end
-          end
-
-        root = File.dirname(root) until File.exist?("#{root}/config.ru")
-        root
-      end
     end
 
-    self.iseq_dir = File.join(find_root, DIRECTORY_NAME)
+    root =
+      caller_locations.detect do |location|
+        path = location.absolute_path || location.path
+        if path != __FILE__ && path !~ %r{bundler[\w.-]*/lib/bundler}
+          break File.dirname(path)
+        end
+      end
+    root = File.dirname(root) until File.exist?("#{root}/config.ru")
+
+    self.iseq_dir = File.join(root, DIRECTORY_NAME)
     FileUtils.mkdir_p(iseq_dir) unless File.directory?(iseq_dir)
   end
 
