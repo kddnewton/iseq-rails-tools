8be4b1d04dd2d5aace337130751f830165c32f66
Kevin Deisz <kevin.deisz@gmail.com>
2017-04-27 20:12:33 -0400

    Provide extension fallback

--- ext/iseq_rails_tools/extconf.rb
+++ ext/iseq_rails_tools/extconf.rb
@@ -1,2 +1,10 @@
-require 'mkmf'
-create_makefile 'iseq_rails_tools/iseq_rails_tools'
+begin
+  require 'mkmf'
+  create_makefile 'iseq_rails_tools/iseq_rails_tools'
+rescue StandardError => error
+  File.open('Makefile', 'wb') do |file|
+    file.puts '.PHONY: install'
+    file.puts 'install:'
+    file.puts "\t" + '@echo "[iseq_rails_tools] not installed, falling back to pure Ruby version."'
+  end
+end
